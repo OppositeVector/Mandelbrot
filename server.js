b30be8f124e9e40756b72b0d9ed5710dd803b257
Victor Belski <iloriginal@gmail.com>
2016-07-23 09:34:21 +0300

    port fix?

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/CalculateFunction', function(req, res) {
 
 });
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('Listening on port ' + port);
